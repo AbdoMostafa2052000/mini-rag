cb970871e2905093a9625f9a41ef09e155397ec8
Abdelrahman Mostafa <abdomostafa2052000@gmail.com>
2026-01-29 22:37:08 +0200

    commit-tut-0.03

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
-app = FastAPI()
+from routes import base ## my .py file
+from dotenv import load_dotenv ## loads .env to my os
 
-@app.get("/welcome")
-def welcome():
-    return {"message": "Hello World!" }
-            
+load_dotenv(".env")
+app = FastAPI()
+app.include_router(base.base_router)
+
+
+
+
+
+
+
+#“When a client requests this URL with this HTTP method, which code should run?”
+#Uvcorn is responsble for runing FastAPI as web
+
+# uvicorn main:app
+#uvicorn main:app --reload --host 0.0.0.0 --port 5000
